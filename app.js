a0a979ce49f363b4d631472ce5fa688cfb2f3a4f
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-07 19:23:41 +0000

    api has been added and tested

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express=require('express');
 const request=require('request')
 const bodyParser=require('body-parser')
+const https=require('https')
 
 const app=express()
 
@@ -17,6 +18,44 @@ app.get('/',(req,res)=>{
   
 })
 app.post('/',function(req,res){
+ 
+    const firstname=req.body.fName;
+    const lastname=req.body.lName;
+    const email=req.body.email;
+    
+    var data={
+        members:[
+            {
+            email_address:email,
+            status:'subscribed',
+            merge_fields:{
+                FNAME:firstname,
+                LNAME:lastname,
+            }
+
+        }
+    ]
+    };
+    const jsonData= JSON.stringify(data)
+
+    const url='https://us1.api.mailchimp.com/3.0/lists/e17cbc3a12'
+    
+    const options={
+        method:"POST",
+        auth:'erdldncr:a064cbab41673d573443bc0cf98eab05-us1'
+    }
+
+  const request=  https.request(url,options,(response)=>{
+        response.on('data',(data)=>{
+            console.log(JSON.parse(data))
+        })
+
+
+    })
+    request.write(jsonData);
+    request.end();
+
+  
 
 })
 
@@ -24,3 +63,8 @@ app.post('/',function(req,res){
 app.listen(3000,function(){
     console.log('server is running on port 3000.')
 })
+
+// a064cbab41673d573443bc0cf98eab05-us1
+
+//list id
+//e17cbc3a12
